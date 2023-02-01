3b62540a143c8e8b48cd4cd2d169f315c253b2d2
Bashkatov-spb <bashkatov.spb@mail.ru>
2023-02-01 11:20:41 +0300

    start project

---
+++ frontend/src/features/App/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App(): JSX.Element {
+  return (
+    <div className="App">
+      <header className="App-header">Our dev</header>
+    </div>
+  );
+}
+
+export default App;
