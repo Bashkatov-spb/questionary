042420313192c014b055ab9e7f00ff5f9bfe6671
Bashkatov-spb <bashkatov.spb@mail.ru>
2023-02-01 11:21:41 +0300

    initialization dev

--- frontend/src/features/App/App.tsx
+++ frontend/src/features/App/App.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 function App(): JSX.Element {
   return (
     <div className="App">
-      <header className="App-header">Our dev</header>
+      <header className="App-header">Next try</header>
     </div>
   );
 }
